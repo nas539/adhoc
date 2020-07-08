84230f371e0309cbe5156f7976bbcfebd3b39d86
Nicholas Sharp <naas8905@gmail.com>
2020-07-08 13:12:39 -0700

    Feature: adding styles to register page

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import Home from './pages/home';
 import Login from './pages/login';
+import Register from './pages/register';
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <Login />
+        <Register />
       </div>
     );
   }
--- src/components/pages/register.js
+++ src/components/pages/register.js
@@ -9,11 +9,26 @@ export default class Register extends Component {
             <div className="register-page-wrapper">
                 <Header />
                 <div className="body-wrapper">
-                    <input type="text" name="username" value="username" placeholder="User Name"/>
-                    <input type="text" name="firstName" value="firstName" placeholder="First Name"/>
-                    <input type="text" name="LastName" value="lastName" placeholder="Last Name"/>
-                    <input type="password" name="password" value="password" placeholder="Enter Password" ></input>
-                    <input type="password" name="confirmPassword" value="confirmPassword" placeholder="Confirm Password"></input>
+                    <div className="register-section">
+                        <p>Username: </p>
+                        <input type="text" name="username" value="username" placeholder="User Name"/>
+                    </div>
+                    <div className="register-section">
+                        <p>First Name: </p>
+                        <input type="text" name="firstName" value="firstName" placeholder="First Name"/>
+                    </div>
+                    <div className="register-section">
+                        <p>Last Name: </p>
+                        <input type="text" name="LastName" value="lastName" placeholder="Last Name"/>
+                    </div>
+                    <div className="register-section">
+                        <p>Password: </p>
+                        <input type="password" name="password" value="password" placeholder="Enter Password" ></input>
+                    </div>
+                    <div className="register-section">
+                        <p>Confirm Password: </p>
+                        <input type="password" name="confirmPassword" value="confirmPassword" placeholder="Confirm Password"></input>
+                    </div>
                     <button>Register</button>
                 </div>
                 <Footer />
