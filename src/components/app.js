e737107dbf4d36fc2ae24acb91d59e37aaccafe4
Nicholas Sharp <naas8905@gmail.com>
2020-07-08 12:52:02 -0700

    Feature: Styling login page

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import Home from './pages/home';
+import Login from './pages/login';
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <Home />
+        <Login />
       </div>
     );
   }
--- src/components/pages/login.js
+++ src/components/pages/login.js
@@ -4,13 +4,28 @@ import Footer from "./footer";
 
 
 export default class Login extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            usernameInput: "",
+            passwordInput: ""
+        }
+    }
+
     render() {
         return (
             <div className="login-page-wrapper">
                 <Header />
                 <div className="body-wrapper">
-                    <input type="text" name="username" value="username"/>
-                    <input type="password" name="password" value="password"></input>
+                    <div className="login-top">
+                    <p>Username: </p>
+                    <input type="text" name="username" value="username" placeholder="Username"/>
+                    </div>
+                    <div className="login-middle">
+                    <p>Password: </p>
+                    <input type="password" name="password" value="password" placeholder="Passwrod"></input>
+                    </div>
                     <button>Login</button>
                 </div>
                 <Footer />
